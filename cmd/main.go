5f0f53f14c6ed94ae1e84d290dc9811da6c55e01
marzhendo <hexsazor@gmail.com>
2026-05-11 22:32:11 +0700

    add booking validation

--- cmd/main.go
+++ cmd/main.go
@@ -1,18 +1,37 @@
 package main
 
-import ("log"
-        "zigzag-barbershop/api"
-		"zigzag-barbershop/config"
-		"zigzag-barbershop/database"
-		"github.com/joho/godotenv"
-	)
+import (
+	"log"
+	"zigzag-barbershop/api"
+	"zigzag-barbershop/config"
+	"zigzag-barbershop/database"
+	"zigzag-barbershop/internal/attendance"
+	"zigzag-barbershop/internal/booking"
+	"zigzag-barbershop/internal/payment"
+	"zigzag-barbershop/internal/user"
+
+	"github.com/joho/godotenv"
+)
 
 func main() {
 	godotenv.Load()
-	
+
 	cfg := config.LoadConfig()
 
 	database.ConnectDB()
+
+	// Migrate database
+	err := database.DB.AutoMigrate(
+		&user.User{},
+		&booking.Booking{},
+		&payment.Payment{},
+		&attendance.Attendance{},
+	)
+	if err != nil {
+		log.Fatalf("Failed to migrate database: %v", err)
+	}
+	log.Println("Database migrated successfully")
+
 	router := api.SetupRouter()
 
 	log.Printf("Starting server on port %s...", cfg.AppPort)
